f6ce0066bcfa72347103bd3bf4b7c5deb0fa6e3b
Franklin.Von <83809201+fvon66@users.noreply.github.com>
2022-01-11 18:28:38 +0800

    批次檔

---
+++ DIP11077002.bat
@@ -0,0 +1,18 @@
+rem 車牌辨識批次作業 -> 寫入 DIP11077002.txt
+Python DIP11077002.py Train01.jpg
+Python DIP11077002.py Train02.jpg
+Python DIP11077002.py Train03.jpg
+Python DIP11077002.py Train04.jpg
+Python DIP11077002.py Train05.jpg
+Python DIP11077002.py Train06.jpg
+Python DIP11077002.py Train07.jpg
+Python DIP11077002.py Train08.jpg
+Python DIP11077002.py Train09.jpg
+Python DIP11077002.py Train10.jpg
+Python DIP11077002.py Train11.jpg
+Python DIP11077002.py Train12.jpg
+Python DIP11077002.py Train13.jpg
+Python DIP11077002.py Train14.jpg
+Python DIP11077002.py Train15.jpg
+Python DIP11077002.py Train16.jpg
+Python DIP11077002.py Train17.jpg
